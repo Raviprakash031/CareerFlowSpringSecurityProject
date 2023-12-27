bdab711e08a9bf8b74a7eb46de3da6b118b4f018
Raviprakash031 <raviprakashreddymolaka@gmail.com>
2023-12-27 22:16:10 +0530

    first Commit

---
+++ CareerFlowBackend1/src/main/java/com/example/demo/CareerFlowBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerFlowBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerFlowBackend1Application.class, args);
+	}
+
+}
